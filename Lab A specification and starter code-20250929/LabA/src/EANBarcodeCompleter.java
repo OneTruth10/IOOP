9ad88d6f523d7b4b5978745db46747246c8b7ccf
OneTruth <firespiritkn10@gmail.com>
2025-09-30 13:44:54 +0100

    IOOP first lab

--- Lab A specification and starter code-20250929/LabA/src/EANBarcodeCompleter.java
+++ Lab A specification and starter code-20250929/LabA/src/EANBarcodeCompleter.java
@@ -8,7 +8,32 @@ public class EANBarcodeCompleter {
         String barcodePrefix = scanner.nextLine();
 
         // Write your solution to Task 1 here
-        
+        boolean validPrefix = true;
+        int temp = 0;
+        if (barcodePrefix.length()==12){
+            
+            for (int i=0; i<12; i++){
+                Character digit = barcodePrefix.charAt(i);
+                if (Character.isDigit(barcodePrefix.charAt(i))){
+                    if (i%2==0){
+                        temp += Character.getNumericValue(digit); 
+                    }else{
+                        temp += Character.getNumericValue(digit)*4;
+                    }
+                }else{
+                    validPrefix = false;
+                }
+            }           
+        } else {
+            validPrefix = false;
+        }
+        if (validPrefix){
+                //System.out.println((10-temp%10)%10);
+                System.out.println("Complete barcode: " + barcodePrefix + (10-temp%10)%10);
+            }else{
+                System.out.println("Invalid barcode prefix");
+            }
+
         scanner.close();
     }
 }
--- Lab A specification and starter code-20250929/LabA/src/IBANValidator.java
+++ Lab A specification and starter code-20250929/LabA/src/IBANValidator.java
@@ -1,5 +1,5 @@
-import java.util.Scanner;
 import java.math.BigInteger;
+import java.util.Scanner;
 
 public class IBANValidator {
     public static void main(String[] args) {
@@ -8,7 +8,34 @@ public class IBANValidator {
         String ibanInput = scanner.nextLine().replaceAll("\\s", "");
 
         // Write your solution to Task 2 here
-
+        boolean validIban = true;
+        String movedString = "";
+        String temp = "";
+        for (int i = 0; i<ibanInput.length(); i++){
+            Character ibanChar = ibanInput.charAt(i);
+            if (!(Character.isDigit(ibanChar) || Character.isUpperCase(ibanChar))){
+                validIban = false;
+                System.out.println("False");
+                break;
+            }
+        }
+        if (validIban){
+            movedString = ibanInput.substring(4, ibanInput.length()) + ibanInput.substring(0,4);
+            for (int i=0; i<movedString.length(); i++){
+                temp += Character.getNumericValue(movedString.charAt(i));
+            }
+            //System.out.println(movedString);
+            //System.out.println(temp);
+            BigInteger bigInt = new BigInteger (temp); 
+            int modValue = bigInt.mod (BigInteger.valueOf (89)).intValue();
+            if (modValue%89==1){
+                System.out.println("Valid IBAN");
+            }else{
+                System.out.println("Invalid IBAN");
+            }
+        }else{
+            System.out.println("Invalid IBAN");
+        }
         scanner.close();
     }
 
